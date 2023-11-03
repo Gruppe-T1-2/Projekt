97229af827b23a7144830b6998f294421b438a9c
Bertil <143800597+Bertildk@users.noreply.github.com>
2023-11-03 17:43:20 +0100

    <3

--- woz/woz/CommandPutDown.java
+++ woz/woz/CommandPutDown.java
@@ -2,27 +2,23 @@ import java.util.Scanner;
 import java.util.*;
 
 public class CommandPutDown extends BaseCommand implements Command{
-    Item item;
+
     InventoryManager inventoryManager;
-    Scanner scan = new Scanner(System.in);
-    String input;
+
+
 
     @Override
     public void execute(Context context, String command, String[] parameters) {
-        System.out.println("Hvilken vare vil du gerne ligge tilbage? ");
-        input = scan.nextLine();
-        try {
-            if (InventoryManager.inventory.get(input).name.equals(input)) {
-                inventoryManager.removeItem(input);
-                System.out.println(input + " Has been removed ");
-
-
-            } else if (!inventoryManager.inventory.get(input).name.equals(input)) {
+        try{
+            if (InventoryManager.getItem(parameters[0]).name.equals(parameters[0])) {
+                inventoryManager.removeItem(parameters[0]);
+                System.out.println(parameters[0] + " Has been removed ");
+            } else if (!InventoryManager.getItem(parameters[0]).equals(parameters[0])) {
                 System.out.println("No such item in inventory");
-            }
-        }catch (NullPointerException Exception){
-            System.out.println("No such item in inventory");
+            }}catch (ArrayIndexOutOfBoundsException e ){
+            System.out.println("Skriv navnet på varen du gerne vil fjerne efter dump ");
         }
+
 
 
     }
